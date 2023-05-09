b35cdcce755aa8a4d6c5035a3be42b9c276b137f
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2023-05-09 16:20:40 -0300

    modificaciones Contenedores.py

--- Contenedores.py
+++ Contenedores.py
@@ -8,7 +8,30 @@ class Medidas:
         self.largoExt = largoExt
         self.anchoExt = anchoExt
         self.altoExt = altoExt
-
+"""
+creo que debemos usar la clase crga, hay una regla de negocio de no cargar una carga que supere la medidas del container
+class Carga:
+    def __init__(self, medidas, precio):
+        self.medidas = madidas
+        self.precio = precio
+        
+class Contenedores(abc.ABC):
+    
+    def __init__(self, id, volumen, peso, medidas):
+        self.id = id
+        self.volumen = volumen
+        self.peso = peso
+        self.medidas = medidas
+    
+    
+    @abc.abstractmethod
+    def getVolumen(self):
+        pass
+        
+    @abc.abstractmethod
+    def cargar(self, carga):
+        pass
+"""
 
 class Contenedores(abc.ABC):
     
