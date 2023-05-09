8267e897fffd525477b8755e3bcbc9cb5f31fb68
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2023-05-09 19:50:40 -0300

    agrego metodo a la clase Barco

--- Vehiculos/Barcos.py
+++ Vehiculos/Barcos.py
@@ -1,7 +1,16 @@
-class Barcos:
-    def __init__(self, id, pesoMax, conteneMax):
+import ABC, abstractmethod
+
+class Barcos(ABC):
+    def __init__(self, id, pesoMax, conteneMax, dimensiones_interior, dimensiones_exterior):
         self.id = id
         self.disponible = True
         self.pesoMax = pesoMax
         self.conteneMax = conteneMax
         self.kmRecorrido = 0
+        self.dimensiones_interior = dimensiones_interior
+        self.dimensiones_exterior = dimensiones_exterior
+        
+    @abstractmethod
+    def cargar(self, carga):
+        #redefinir en cada clase que ereda de barco
+        pass
