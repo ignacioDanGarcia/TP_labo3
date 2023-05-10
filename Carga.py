2a15ba8a420557cf61c20ea855fcd6f24d51bf4b
abril <abrilalvarez1205@gmail.com>
2023-05-10 13:04:29 -0300

    modificacion

--- Carga.py
+++ Carga.py
@@ -1,5 +1,10 @@
 class Carga:
 
-    def __init__(self, medidas, precio):
+    def __init__(self, medidas):
         self.medidas = medidas
-        self.precio = precio
+        self.precio = 0.0
+
+    def definir_precio_carga(self):
+        #se define segun la carga
+        #esto creo debe estar en otra clase para implementar principio de responsabiliddad unica
+        pass
