986a66c5132473406ffbac48ac718a9584d08df2
Ignacio Nicolas Garcia <ignacio1garcia2nacho@gmail.com>
2023-05-15 00:56:15 -0300

    .

--- Barcos_directorio/Excepciones/Contenedor_no_aceptado_exception.py
+++
@@ -1,5 +0,0 @@
-class Contenedor_no_aceptado_exception(Exception):
-    def __init__(self, mensaje) -> None:
-        self.mensaje = mensaje
-    def __str__(self) -> str:
-        return self.mensaje
--- Barcos_directorio/Excepciones/Peso_excedido_exception.py
+++
@@ -1,5 +0,0 @@
-class Peso_excedido_exception(Exception):
-    def __init__(self, mensaje) -> None:
-        self.mensaje = mensaje
-    def __str__(self) -> str:
-        return self.mensaje
