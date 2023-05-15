c738511275f34b12805e401366b34269b5de7e4a
Ignacio Nicolas Garcia <ignacio1garcia2nacho@gmail.com>
2023-05-15 00:55:50 -0300

    Mejora a las excepciones en barco y métodos

--- Barcos_directorio/Barco_basico.py
+++ Barcos_directorio/Barco_basico.py
@@ -1,15 +1,24 @@
 from Barcos_directorio.Barcos import Barco
 from Cont_basico_interfaz import Cont_basico_interfaz
-from Barcos_directorio.Excepciones.Contenedor_no_aceptado_exception import Contenedor_no_aceptado_exception
-from Barcos_directorio.Excepciones.Peso_excedido_exception import Peso_excedido_exception
+from Barcos_directorio.Excepciones.exceptions import *
+
 
 class Barco_basico(Barco):
     def __init__(self, id, peso_max, cant_contenedores_max, lleva_mat_esp):
         super().__init__(id, peso_max, cant_contenedores_max, lleva_mat_esp)
     
-    def puede_cargar(self, carga):
+    def puede_cargar_esta_carga(self, carga):
         if not (isinstance(carga, Cont_basico_interfaz)):
             raise Contenedor_no_aceptado_exception("Este barco solo puede llevar contenedores básicos")
-        else:
-            if ( carga.get_peso() + self.obtener_peso_actual() )> self.peso_max:
-                raise Peso_excedido_exception(f"Este peso es mucho para el barco. Sobran {self.obtener_peso_actual() + carga.get_peso() - self.peso_max} kgs")
+        #Hay que ver como manejamos en la carga si lleva alguna carga especial o no.
+        if not self.lleva_mat_esp and carga.get_mat_especial():
+            raise Material_no_compatible_con_barco_Exceptionn("Este barco no puede llevar el material que se intenta cargar.")
+        return True
+        
+    def tiene_lugar(self, carga):
+        if ( carga.get_peso() + self.obtener_peso_actual() )> self.peso_max:
+            raise Peso_excedido_exception(f"Este peso es mucho para el barco. Sobran {self.obtener_peso_actual() + carga.get_peso() - self.peso_max} kgs")
+        
+        if self.contenedores.size() == self.cant_contenedores_max:
+            raise Cantidad_contenedores_maxima_alcanzada_exception("El barco está lleno. No es posible cargar el contenedor.")
+    
