4f8acfd3fbfea35efe86e574cc58fab890f84da5
Ignacio Daniel García <igniciagar@gmail.com>
2023-05-16 17:29:09 -0300

    agrego funcion de calcular precio adicional (tabla precios)

--- BarcosDirectorio/Excepciones/exceptions.py
+++ BarcosDirectorio/Excepciones/exceptions.py
@@ -32,3 +32,9 @@ class No_hay_camiones_disponibles(Exception):
         self.mensaje = mensaje
     def __str__(self) -> str:
         return self.mensaje
+    
+class distancia_incorrecta(Exception):
+    def __init__(self, mensaje) -> None:
+        self.mensaje = mensaje
+    def __str__(self) -> str:
+        return self.mensaje
