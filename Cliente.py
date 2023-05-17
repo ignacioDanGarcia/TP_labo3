a133843bd14af91d1d36cb50244bb8f7c748dd4d
Ignacio Nicolas Garcia <ignacio1garcia2nacho@gmail.com>
2023-05-17 11:05:51 -0300

    Info en Cliente

--- Cliente.py
+++ Cliente.py
@@ -2,6 +2,26 @@
 
 class Cliente:
     def __init__(self, apellido, nombre, id):
-        self.apellido = apellido
-        self.nombre = nombre
-        self.id = id
+        self.__apellido = apellido
+        self.__nombre = nombre
+        self.__id = id
+    
+    'Getters y Setters:'
+    def get_apellido(self):
+        return self.__apellido
+    def set_apellido(self,a):
+        self.__apellido = a
+    apellido = property(get_apellido,set_apellido)
+    
+    def get_nombre(self):
+        return self.__nombre
+    def set_nombre(self,nombre):
+        self.__nombre = nombre
+    nombre = property(get_nombre,set_nombre)
+    
+    def get_id(self):
+        return self.__id
+    def set_id(self,id):
+        self.__id = id
+    id = property(get_id,set_id)
+    
