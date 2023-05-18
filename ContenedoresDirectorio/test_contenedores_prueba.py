27c0eff46d2fecfb187991e126a440877d7368ba
Ignacio Nicolas Garcia <ignacio1garcia2nacho@gmail.com>
2023-05-18 00:43:56 -0300

    Testing de contenedores corregido

--- ContenedoresDirectorio/test_contenedores_prueba.py
+++ ContenedoresDirectorio/test_contenedores_prueba.py
@@ -13,23 +13,20 @@ class test_empresa(TestCase):
     """ 5. Cualquier carga cuyas dimensiones, volumen o peso supere lo definido en el container no podrá ser trasladada en el mismo.
     6. Un contenedor sin características especiales no puede transportar material especial. """
         
-    '''def test_verificar_carga_por_dimensiones1(self):
-        conteiner_hc = BasicoHCContenedor(3,True)
-        medidas = Medidas(3,3,5)
-        mock_carga = Mock()
-        mock_carga.get_medidas.return_value = medidas
-        mock_carga.get_peso.return_value = 50
-        assert(conteiner_hc.verificar_carga(mock_carga)) == True
-        # falta implementar
-        pass'''
+    def test_verificar_carga_por_dimensiones1(self):
+        conteiner_hc = BasicoHCContenedor(3, False)
+        medidas = Medidas(100,3,5)
+        carga = Carga(medidas,50,None)
+        with self.assertRaises(contenedor_no_puede_llevar_carga):
+            conteiner_hc.verificar_carga(carga)
+        'El volumen es superior'
     
     def test_verificar_carga_por_dimensiones2(self):
-        conteiner_hc = BasicoHCContenedor(3, None)
-        medidas = Medidas(3,3,5)
-        carga = Carga(medidas,50,30,False)
+        conteiner_hc = BasicoHCContenedor(3, False)
+        medidas = Medidas(3,2,2)
+        carga = Carga(medidas,50,None)
         assert(conteiner_hc.verificar_carga(carga)) == True
-        # falta implementar
-        pass
+    
     
     #def test_verificar_carga_por_mat_especial(self):
         # falta implementar
