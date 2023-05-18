db41b24148ac4d41fc8b746553b4ee18039271e6
Ignacio Daniel García <igniciagar@gmail.com>
2023-05-17 21:42:26 -0300

    arreglo funcion de definir precio adicional

--- Carga.py
+++ Carga.py
@@ -1,19 +1,11 @@
 from Medidas import Medidas
 class Carga:
 
-    def __init__(self, medidas, peso):
+    def __init__(self, medidas, peso, precio):
         self.__medidas = medidas
         self.__precio = 0.0
         self.__peso = peso
-
-    def definir_precio_carga(self):
-        #se define segun la carga: carga especial o simple
-        #esto creo debe estar en otra clase para implementar principio de responsabiliddad unica
-        
-        #Buenis, donde dice que depende del tipo de carga? En el apartado de contenedores solo vo que dice que se suma el precio a pagar por la carga misma. 
-        # Entiendo que la carga tendría un precio del producto que está adentro en sí y después se le suma al del contenedor por hacer el viaje.
-        # El peso de la carga sería fijo y el del contenedor varía según la tabla de precios.
-        pass
+        self.__precio = precio
     
     'Getters y setters'
     def get_medidas(self):
@@ -33,3 +25,10 @@ class Carga:
     def set_peso(self, peso):
         self.__peso = peso
     peso = property(get_peso,set_peso)
+
+    def get_precio(self):
+        return self.__precio
+    def set_precio(self, p):
+        self.__precio = p
+    precio = property(get_precio,set_precio)
+    
