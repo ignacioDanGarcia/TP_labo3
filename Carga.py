b9783d8de066726d69feb4dc8c69d102ce497229
Ignacio Daniel García <igniciagar@gmail.com>
2023-05-18 00:14:08 -0300

    me olvide del constructor de carga

--- Carga.py
+++ Carga.py
@@ -3,11 +3,12 @@ from Tasador_de_cargas import Tasador_de_cargas
 
 class Carga:
 
-    def __init__(self, medidas, peso, precio, mat_especial):
+    def __init__(self, medidas, peso, mat_especial):
+        tas = Tasador_de_cargas()
+        p = tas.setear_precio_carga()
         self.__medidas = medidas
-        self.__precio = 0
+        self.__precio = p
         self.__peso = peso
-        self.__precio = precio
         self.__mat_especial = mat_especial
     
     'Getters y setters'
@@ -28,12 +29,6 @@ class Carga:
     def set_peso(self, peso):
         self.__peso = peso
     peso = property(get_peso,set_peso)
-
-    def get_precio(self):
-        return self.__precio
-    def set_precio(self, p):
-        self.__precio = p
-    precio = property(get_precio,set_precio)
     
     def get_mat_especial(self):
         return self.__mat_especial
