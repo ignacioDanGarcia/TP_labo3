a3dfc11a798a15d08fbb7c3ed7cf9b3adcde693b
Ignacio Nicolas Garcia <ignacio1garcia2nacho@gmail.com>
2023-05-17 23:52:08 -0300

    Agrego test en contenedores y modifico metodos.

--- ContenedoresDirectorio/Contenedor_basico_estandar.py
+++ ContenedoresDirectorio/Contenedor_basico_estandar.py
@@ -4,8 +4,8 @@ from Medidas import Medidas
 
 
 class Contenedor_Basico_Estandar(Cont_basico_abstracto):
-    def __init__(self,id, precio_transporte):
-        super().__init__(id, precio_transporte)
+    def __init__(self,id, mat_especial):
+        super().__init__(id, mat_especial)
         self.medidas_exterior = Medidas(6.1,2.45,2.6)
         self.medidas_interior = Medidas(6.0,2.35,2.3)
         self.peso_max = 24000
--- ContenedoresDirectorio/test_contenedores_prueba.py
+++ ContenedoresDirectorio/test_contenedores_prueba.py
@@ -1,6 +1,8 @@
+from types import NoneType
 from unittest import TestCase
 from unittest.mock import Mock, patch, MagicMock
 import mock
+from Carga import Carga
 from ContenedoresDirectorio.BasicoHCContenedor import BasicoHCContenedor
 from pytest import raises
 from Excepciones.exceptions import *
@@ -11,15 +13,24 @@ class test_empresa(TestCase):
     """ 5. Cualquier carga cuyas dimensiones, volumen o peso supere lo definido en el container no podrá ser trasladada en el mismo.
     6. Un contenedor sin características especiales no puede transportar material especial. """
         
-    def test_verificar_carga_por_dimensiones1(self):
-        barco_hc = BasicoHCContenedor(3,True)
+    '''def test_verificar_carga_por_dimensiones1(self):
+        conteiner_hc = BasicoHCContenedor(3,True)
         medidas = Medidas(3,3,5)
         mock_carga = Mock()
         mock_carga.get_medidas.return_value = medidas
-        #mock_carga.get_peso.return_value = 50
-        assert(barco_hc.verificar_carga(mock_carga)) == True
+        mock_carga.get_peso.return_value = 50
+        assert(conteiner_hc.verificar_carga(mock_carga)) == True
+        # falta implementar
+        pass'''
+    
+    def test_verificar_carga_por_dimensiones2(self):
+        conteiner_hc = BasicoHCContenedor(3, None)
+        medidas = Medidas(3,3,5)
+        carga = Carga(medidas,50,30,False)
+        assert(conteiner_hc.verificar_carga(carga)) == True
         # falta implementar
         pass
+    
     #def test_verificar_carga_por_mat_especial(self):
         # falta implementar
     #    pass
