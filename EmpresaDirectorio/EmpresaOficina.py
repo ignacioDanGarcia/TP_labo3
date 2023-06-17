35dfaa768b3bbeec6173f5b17a880b4e84dfbc02
Ignacio Daniel García <igniciagar@gmail.com>
2023-06-17 19:40:20 -0300

    agrego comentarios con la explicacion de la logica de procesamiento de pedido sin codigo

--- EmpresaDirectorio/EmpresaOficina.py
+++ EmpresaDirectorio/EmpresaOficina.py
@@ -22,8 +22,46 @@ class EmpresaOficina():
     empresa_data = property(get_empresa_data,set_empresa_data)
     
     
-    def calcular_precio_pedido(self, pedido, distancia):
-        pass
+    # lo que me falta para escribir el metodo es:
+    # ver si el cliente quiere camion o no,
+    # si quiere barco, o las dos cosas
+    # y si todo sale bien, mandarle vehiculos a EmpresaEnvios para que ejecute viajar
     
     def procesar_pedido(self, pedido: Pedidos):
+        # pedirle a empresa deposito que traten de llenar los contenedores
+        # chequeando las cargas con los contenedores que tiene la empresa, en empresa data
+        
+        # guardas los contenedores usados en pedido.contenedores
+        
+        # si sale bien chequear si tenemos los vehiculos del tipo de viaje que quiere
+        
+        # si sale bien los contenedores en los barcos o camiones, con los que estan en empresa data
+        
+        # si sale bien, ejecutas self.calcular_precio_pedido que es el metodo de abajo
+        
+        
         pass
+    
+    
+    def calcular_precio_pedido(self, pedido, distancia):
+        # vas a empresa cotizaciones y haces lo siguiente:
+        
+        # pedido_cargas = pedido.get_cargas()
+        """
+        for contenedor in pedido.contenedores: # que son los contenedores que va a utilizar ese pedido
+            # le pasamos todas las cargas del cliente y calcula solo con las que estan en el contenedor
+            precio_pedido = empresa_cotizaciones.calcular_precio(contenedor, pedido.get_distancia(), pedido_cargas)
+            
+            # aca habria que buscar la forma de remover de pedido_cargas las cargas que se van usando
+            # asi al final le devolvemos el precio solo si esa lista de pedido_cargas esta vacia
+            # como para liberar una excepcion porque algo salio mal, y no le dimos el precio de todas las cargas
+            # creo que nunca pasaria pero bueno es una idea
+        
+        pedido.set_precio_final_pedido(precio_pedido)
+        
+        return # nada porque ya se guarda en pedido
+        """
+        
+        pass
+    
+        # HABRIA QUE VER COMO HACEMOS PARA MANDARLE LOS BARCOS Y CAMIONES CARGADOS A EMPRESA ENVIOS PARA QUE LOS HAGA VIAJAR
