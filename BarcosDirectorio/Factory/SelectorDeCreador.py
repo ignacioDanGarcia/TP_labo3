69864044bd2f15f6c468622098e19fa265c94407
Ignacio Nicolas Garcia <ignacio1garcia2nacho@gmail.com>
2023-06-21 20:21:49 -0300

    Cambiamos la factoria de tipos de barcos a una factoria del barco normla y que le pasas a la factoria el tipo de barco que queres crear. De esta manera la clase de barcos no tiene responsabilidades duplicadas y lo manejamos con un manejador de contenedores haciendo un strategy. Agrego tests y edito los anteriores.

--- BarcosDirectorio/Factory/SelectorDeCreador.py
+++ BarcosDirectorio/Factory/SelectorDeCreador.py
@@ -1,14 +1,13 @@
+from BarcosDirectorio.Factory.CreadorBarcos import CreadorDeBarcos
 from BarcosDirectorio.TiposDeBarcos import TiposBarcos
-from BarcosDirectorio.Factory.CreadorDeBarcosBasicos import CreadorBarcosBasicos
-from BarcosDirectorio.Factory.CreadorDeBarcosEspeciales import CreadorBarcosEspeciales
 from Excepciones.exceptions import TipoDeBarcoInvalido
 class SelectorCreador():
     def __init__(self) -> None:
         pass
     
-    def crear_factoria(self, tipoBarco :TiposBarcos):
-        if tipoBarco == tipoBarco.BASICO:
-            return CreadorBarcosBasicos()
-        if tipoBarco == tipoBarco.ESPECIAL:
-            return CreadorBarcosEspeciales()
+    def crear_factoria_de_tipo_de_barco(self, tipoBarco :TiposBarcos):
+        if tipoBarco == tipoBarco.BASICO or tipoBarco == tipoBarco.ESPECIAL:
+            creador = CreadorDeBarcos(tipoBarco)
+            return creador
+        
         raise TipoDeBarcoInvalido("No hay una factoría para este tipo de barco.")
--- MockObjects/Tests/test_CreacionBarcos.py
+++ MockObjects/Tests/test_CreacionBarcos.py
@@ -1,9 +1,7 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch, MagicMock
-from BarcosDirectorio.BarcoBasico import BarcoBasico
-from BarcosDirectorio.BarcoEspecial import BarcoEspecial
-from BarcosDirectorio.Factory.CreadorDeBarcosBasicos import CreadorBarcosBasicos
-from BarcosDirectorio.Factory.CreadorDeBarcosEspeciales import CreadorBarcosEspeciales
+
+from BarcosDirectorio.Barcos import Barco
 from BarcosDirectorio.Factory.SelectorDeCreador import SelectorCreador
 from BarcosDirectorio.TiposDeBarcos import TiposBarcos
 
@@ -11,19 +9,22 @@ class test_creacion_de_barcos_con_factory_method(TestCase):
     def test_creacion_de_barco_basico(self):
         selector_de_factoria = SelectorCreador()
         
-        creador_barcos_basicos = selector_de_factoria.crear_factoria(TiposBarcos.BASICO)
+        creador_barcos_basicos = selector_de_factoria.crear_factoria_de_tipo_de_barco(TiposBarcos.BASICO)
         
         barco = creador_barcos_basicos.crear_barco(1,500,3,500)
-        assert isinstance(barco,BarcoBasico)
+        assert isinstance(barco,Barco)
+        assert barco.get_tipo_barco() == TiposBarcos.BASICO
+
         assert barco.get_peso_max() == 500
         assert barco.get_cant_contenedores_max() == 3
     
     def test_creacion_de_barco_especial(self):
         selector_de_factoria = SelectorCreador()
-        creador_barcos_especiales = selector_de_factoria.crear_factoria(TiposBarcos.ESPECIAL)
+        creador_barcos_especiales = selector_de_factoria.crear_factoria_de_tipo_de_barco(TiposBarcos.ESPECIAL)
         
         barco = creador_barcos_especiales.crear_barco(1,500,3,500)
-        assert isinstance(barco,BarcoEspecial)
+        assert isinstance(barco,Barco)
+        assert barco.get_tipo_barco() == TiposBarcos.ESPECIAL
         assert barco.get_peso_max() == 500
         assert barco.get_cant_contenedores_max() == 3
         
